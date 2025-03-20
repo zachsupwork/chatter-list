76a45279fa0e95f45e786551fbb86afbcb528106
zachsupwork <zachsupwork@gmail.com>
2025-03-20 15:10:19 -0400

    Update retell.ts

--- src/lib/retell.ts
+++ src/lib/retell.ts
@@ -1,5 +1,5 @@
 
-export const RETELL_API_KEY = 'key_bc69ed16c81fa347d618b4763cb7';
+export const RETELL_API_KEY = 'key_59b1dda9a8189ac7f7a8a7dc89c0';
 // export const RetellConfig = {
 //   headers: {
 //     'Authorization': `Bearer ${RETELL_API_KEY}`,
